1121dc66a6d024964db90efc9cc9001a8ee347d6
Marcos Gustavo Mendonça Pereira <marcosgustavomp2@gmail.com>
2026-04-10 16:00:48 -0300

    Delete NaturalStore/NaturalStore directory

--- NaturalStore/NaturalStore/Domain/Enums/Enums.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Enums;
-
-public enum ContractType { CLT, CNPJ }
-
-public enum EmployeeRole { Cashier, Stocker, Manager, Deliverer }
-
-public enum PaymentMethod { CreditCard, DebitCard, Cash }
-
-public enum SaleUnit { Unit, Kilo }
